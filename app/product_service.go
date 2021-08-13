207fe30a6925c23333c14090377f5a30b639ebaf
Charles Willian <wcharlesw15@gmail.com>
2021-08-13 20:24:00 -0300

    feat: adds service and unit tests

---
+++ app/product_service.go
@@ -0,0 +1,14 @@
+package app
+
+type ProductService struct {
+	Persistence ProductPersistenceInterface
+}
+
+func (s *ProductService) Get(id string) (ProductInterface, error) {
+	product, err := s.Persistence.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
---
+++ app/product_service_test.go
@@ -0,0 +1,27 @@
+package app_test
+
+import (
+	"testing"
+
+	"github.com/charles00willian/hexagonal-arch-go-starter/app"
+	"github.com/charles00willian/hexagonal-arch-go-starter/app/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_app.NewMockProductInterface(ctrl)
+	persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := app.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
